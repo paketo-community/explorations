b1a24a7416e64eaececd2a0d9de57d72244ffffb
Frankie Gallina-Jones <frankieg@vmware.com>
2022-01-25 17:38:56 -0500

    add deploy script and workflow

--- triage-party/README.md
+++ triage-party/README.md
@@ -5,7 +5,7 @@ This directory contains configuration and scripts that deploy an instance of
 repositories.
 
 ## Viewing/Using Triage Party
-The instance can be viewed here: <URL>
+The current instance can be viewed here: https://triage-party-ngfy5sbarq-uc.a.run.app/s/weekly
 
 ## Deploying Triage Party
 
@@ -15,3 +15,10 @@ backend to persist some data. The workflow
 builds a triage party Docker image using [`config.yml`](./config.yml), pushes
 it to a Paketo GCR repository, and deploys the image to Google Cloud Run,
 connected to its database backend.
+
+To deploy a fresh Triage Party instance:
+1. Create and push a new triage-party image.
+1. Create Google Cloud SQL MySQL instance. Add an empty database for triage
+   party data to that instance.
+1. Run `./deploy.sh --init` with the appropriate environment variables
+   configured to create the new service.
--- triage-party/deploy.sh
+++ triage-party/deploy.sh
@@ -3,15 +3,24 @@
 set -eu
 set -o pipefail
 
-readonly PROGDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly DB_INSTANCE="${DB_INSTANCE:-triage-party}"
+readonly DB_NAME="${DB_NAME:-tpdb}"
+readonly DB_REGION="${DB_REGION:-us-central1}"
+readonly CLOUD_RUN_REGION="${CLOUD_RUN_REGION:-us-central1}"
 
 function main(){
+  local initial="false"
   while [[ "${#}" != 0 ]]; do
     case "${1}" in
       --help|-h)
         shift 1
         usage
         exit 0
+        ;;
+
+      --init|-i)
+        shift 1
+        initial="true"
         ;;
 
       "")
@@ -25,29 +34,42 @@ function main(){
   done
 
   check_env_vars
+  if [[ "${initial}" == "true" ]]; then
+    gcloud run deploy triage-party \
+      --project "${GCP_PROJECT_ID}" \
+      --region "${CLOUD_RUN_REGION}" \
+      --image "${IMAGE}" \
+      --set-env-vars="GITHUB_TOKEN=${GITHUB_TOKEN},PERSIST_BACKEND=cloudsql,PERSIST_PATH=${DB_USER}:${DB_PASS}@tcp(${GCP_PROJECT_ID}/${DB_REGION}/${DB_INSTANCE})/${DB_NAME}" \
+      --allow-unauthenticated \
+      --platform managed
+    exit 0
+  fi
 
-  gcloud beta run deploy triage-party \
-    --project "${PROJECT_ID}" \
-    --image "${IMAGE}" \
-    --set-env-vars="GITHUB_TOKEN=${GITHUB_TOKEN},PERSIST_BACKEND=cloudsql,PERSIST_PATH=${DB_USER}:${DB_PASS}@tcp(${PROJECT_ID}/us-central1/triage-party)/tp" \
-        --allow-unauthenticated \
+  gcloud run services update triage-party \
+    --project "${GCP_PROJECT_ID}" \
     --region us-central1 \
-    --platform managed
+    --image "${IMAGE}" \
+    --set-env-vars="GITHUB_TOKEN=${GITHUB_TOKEN},PERSIST_BACKEND=cloudsql,PERSIST_PATH=${DB_USER}:${DB_PASS}@tcp(${GCP_PROJECT_ID}/${DB_REGION}/${DB_INSTANCE})/${DB_NAME}"
 }
 
 function usage() {
   cat <<-USAGE
 deploy.sh [OPTIONS]
 
-Deploys the Paketo triage party instance with collocated config.
+Updates the Paketo triage party instance with a prebuilt image.
+
 Script requires that the following environment variables are set:
   DB_PASS - The password to access the Google Cloud SQL instance
   DB_USER - The username to access the Google Cloud SQL instance
   GITHUB_TOKEN – The token that will be used to scrape Github data
   IMAGE - The URI of the triage party image to deploy
 
+A CloudSQL MySQL instance for persisting Triage Party data must already exist
+with the name '${DB_INSTANCE}' and contain a database called '${DB_NAME}'.
+
 OPTIONS
   --help       -h  prints the command usage
+  --init       -i  performs an initial gcloud run deploy
 
 USAGE
 }
@@ -70,6 +92,17 @@ function check_env_vars() {
 
   if [[ -z "${IMAGE-}" ]]; then
     echo -e "Environment variable \$IMAGE is required" >&2
+    exit 1
+  fi
+
+  if [[ -z "${DB_IP-}" ]]; then
+    echo -e "Environment variable \$DB_IP is required" >&2
+    exit 1
+  fi
+
+  if [[ -z "${GCP_PROJECT_ID-}" ]]; then
+readonly GCP_PROJECT_ID="${GCP_PROJECT_ID:-paketo-community}"
+    echo -e "Environment variable \$GCP_PROJECT_ID is required" >&2
     exit 1
   fi
 }
