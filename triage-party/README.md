69ab71da8d81214865ed48358809238f126e7070
Tim Hitchener <thitch97@users.noreply.github.com>
2022-01-26 10:30:36 -0500

    Add Paketo triage-party config (#2)
    * Add experimental Paketo triage-party config
    
    * create deploy script and workflow
    
    * add deploy script and workflow
    
    * redeploy on changes to subdirectory
    
    Co-authored-by: Frankie Gallina-Jones <frankieg@vmware.com>

---
+++ triage-party/README.md
@@ -0,0 +1,30 @@
+# Triage Party
+
+This directory contains configuration and scripts that deploy an instance of
+[triage party](https://github.com/google/triage-party) for Paketo's
+repositories.
+
+## Viewing/Using Triage Party
+The current instance can be viewed here: https://triage-party-ngfy5sbarq-uc.a.run.app/s/weekly
+
+## Deploying Triage Party
+
+The triage party instance is hosted with Google Cloud Run. It uses a MySQL
+backend to persist some data. The workflow
+[`.github/workflows/deploy-triage-party.yml`](../.github/workflows/deploy-triage-party.yml)
+builds a triage party Docker image using [`config.yml`](./config.yml), pushes
+it to a Paketo GCR repository, and deploys the image to Google Cloud Run,
+connected to its database backend.
+
+### To redeploy the existing Triage Party with an updated config:
+1. Push changes to [`config.yml`](./config.yml) on `main`
+1. The redeploy workflow should trigger automatically
+Alternately, manually trigger the workflow.
+
+
+### To deploy a fresh Triage Party instance:
+1. Create and push a new triage-party image.
+1. Create Google Cloud SQL MySQL instance. Add an empty database for triage
+   party data to that instance.
+1. Run `./deploy.sh --init` with the appropriate environment variables
+   configured to create the new service.
---
+++ triage-party/deploy.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+readonly DB_INSTANCE="${DB_INSTANCE:-triage-party}"
+readonly DB_NAME="${DB_NAME:-tpdb}"
+readonly DB_REGION="${DB_REGION:-us-central1}"
+readonly CLOUD_RUN_REGION="${CLOUD_RUN_REGION:-us-central1}"
+
+function main(){
+  local initial="false"
+  while [[ "${#}" != 0 ]]; do
+    case "${1}" in
+      --help|-h)
+        shift 1
+        usage
+        exit 0
+        ;;
+
+      --init|-i)
+        shift 1
+        initial="true"
+        ;;
+
+      "")
+        # skip if the argument is empty
+        shift 1
+        ;;
+
+      *)
+        util::print::error "unknown argument \"${1}\""
+    esac
+  done
+
+  check_env_vars
+  if [[ "${initial}" == "true" ]]; then
+    gcloud run deploy triage-party \
+      --project "${GCP_PROJECT_ID}" \
+      --region "${CLOUD_RUN_REGION}" \
+      --image "${IMAGE}" \
+      --set-env-vars="GITHUB_TOKEN=${GITHUB_TOKEN},PERSIST_BACKEND=cloudsql,PERSIST_PATH=${DB_USER}:${DB_PASS}@tcp(${GCP_PROJECT_ID}/${DB_REGION}/${DB_INSTANCE})/${DB_NAME}" \
+      --allow-unauthenticated \
+      --platform managed
+    exit 0
+  fi
+
+  gcloud run services update triage-party \
+    --project "${GCP_PROJECT_ID}" \
+    --region us-central1 \
+    --image "${IMAGE}" \
+    --set-env-vars="GITHUB_TOKEN=${GITHUB_TOKEN},PERSIST_BACKEND=cloudsql,PERSIST_PATH=${DB_USER}:${DB_PASS}@tcp(${GCP_PROJECT_ID}/${DB_REGION}/${DB_INSTANCE})/${DB_NAME}"
+}
+
+function usage() {
+  cat <<-USAGE
+deploy.sh [OPTIONS]
+
+Updates the Paketo triage party instance with a prebuilt image.
+
+Script requires that the following environment variables are set:
+  DB_PASS - The password to access the Google Cloud SQL instance
+  DB_USER - The username to access the Google Cloud SQL instance
+  GITHUB_TOKEN – The token that will be used to scrape Github data
+  IMAGE - The URI of the triage party image to deploy
+
+A CloudSQL MySQL instance for persisting Triage Party data must already exist
+with the name '${DB_INSTANCE}' and contain a database called '${DB_NAME}'.
+
+OPTIONS
+  --help       -h  prints the command usage
+  --init       -i  performs an initial gcloud run deploy
+
+USAGE
+}
+
+function check_env_vars() {
+  if [[ -z "${GITHUB_TOKEN-}" ]]; then
+    echo -e "Environment variable \$GITHUB_TOKEN is required" >&2
+    exit 1
+  fi
+
+  if [[ -z "${DB_USER-}" ]]; then
+    echo -e "Environment variable \$DB_USER is required" >&2
+    exit 1
+  fi
+
+  if [[ -z "${DB_PASS-}" ]]; then
+    echo -e "Environment variable \$DB_PASS is required" >&2
+    exit 1
+  fi
+
+  if [[ -z "${IMAGE-}" ]]; then
+    echo -e "Environment variable \$IMAGE is required" >&2
+    exit 1
+  fi
+
+  if [[ -z "${DB_IP-}" ]]; then
+    echo -e "Environment variable \$DB_IP is required" >&2
+    exit 1
+  fi
+
+  if [[ -z "${GCP_PROJECT_ID-}" ]]; then
+    echo -e "Environment variable \$GCP_PROJECT_ID is required" >&2
+    exit 1
+  fi
+}
+
+main "${@:-}"
